e0a0018383f800273a8564a5f4bd98c625ce18a0
srikant sistla <40070893+suddeninterruption@users.noreply.github.com>
2018-07-30 17:25:07 -0700

    Create wordcloud.py

---
+++ wordcloud.py
@@ -0,0 +1,31 @@
+import numpy as np # linear algebra
+import pandas as pd 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+from subprocess import check_output
+from wordcloud import WordCloud, STOPWORDS
+
+mpl.rcParams['font.size']=12                #10 
+mpl.rcParams['savefig.dpi']=100             #72 
+mpl.rcParams['figure.subplot.bottom']=.1 
+
+
+stopwords = set(STOPWORDS)
+data = pd.read_csv("rand_reviews.csv")
+
+wordcloud = WordCloud(
+                          background_color='white',
+                          stopwords=stopwords,
+                          max_words=200,
+                          max_font_size=40, 
+                          random_state=42
+                         ).generate(str(data['title']))
+
+print(wordcloud)
+fig = plt.figure(1)
+plt.imshow(wordcloud)
+plt.axis('off')
+plt.show()
+fig.savefig("word1.png", dpi=900)
